1a0fee35ec71c909ebacd5c3b9caaee6bddb72d5
Jan Daniel Andersen (JAXX) <jaxx@oticon.com>
2019-04-11 08:48:05 +0200

    First

---
+++ MyLib/LoggingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyLib
+{
+    public interface ILoggingService
+    {
+        void Log(string message);
+    }
+
+    internal class LoggingService : ILoggingService
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
